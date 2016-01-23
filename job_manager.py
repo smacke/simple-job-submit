b3da632d863795ad0021f7e94b43aa35787f2d01
Stephen Macke <smacke@cs.stanford.edu>
2016-01-23 17:40:29 -0600

    add example config, impl lots of features

--- job_manager.py
+++ job_manager.py
@@ -13,6 +13,7 @@ pipe_name = 'jobs.pipe'
 max_jobs = 4
 jobs_running = 0
 jobs = []
+current_job_id = 0
 jobs_cv = threading.Condition(threading.Lock())
 commands_q = Queue.Queue()
 
@@ -27,16 +28,25 @@ def sigchld_handler(signum, frame):
     saturated.notify()
     saturated.release()
 
+def prehooks(cmd_json):
+    global jobs_running
+
+    if cmd_json['git']:
+        saturated.acquire()
+        jobs_running += 1 # need to increment this because sigchild handler will dec
+        saturated.release()
+        subprocess.call(shlex.split('git pull'))
+
+    if cmd_json['make']:
+        saturated.acquire()
+        jobs_running += 1 # need to increment this because sigchild handler will dec
+        saturated.release()
+        subprocess.call(['make'])
+
 def run_jobs():
     global jobs_running
     global jobs
     while True:
-        saturated.acquire()
-        while jobs_running == max_jobs:
-            saturated.wait()
-        jobs_running += 2 # increment by two since the 'git pull' will decrement
-        saturated.release()
-
         jobs_cv.acquire()
         while len(jobs)==0:
             jobs_cv.wait()
@@ -44,32 +54,82 @@ def run_jobs():
         jobs = jobs[1:]
         jobs_cv.release()
 
-        subprocess.call(shlex.split('git pull'))
-        subprocess.Popen(job, shell=True)
+        saturated.acquire()
+        while jobs_running >= max_jobs:
+            saturated.wait()
+        jobs_running += 1
+        saturated.release()
+
+        subprocess.Popen(job['run'], shell=True)
+
+def handle_submit_job(command):
+    global current_job_id
+    jobs_cv.acquire()
+    jobs.append(command)
+    jid = current_job_id
+    current_job_id += 1
+    jobs_cv.notify()
+    jobs_cv.release()
+    ret = {'code': 0, 'status': 'OK', 'job_id': jid, 'message': 'job submitted successfully'}
+    with open(command['port'], 'w') as f:
+        f.write(json.dumps(ret))
+
+def handle_get_listing(command):
+    global max_jobs
+    global jobs_running
+    global jobs
+    jobs_cv.acquire()
+    queued = str(jobs)
+    jobs_cv.release()
+    running = jobs_running
+    ret = {'code': 0, 'status': 'OK', 'jobs_running': running, 'jobs_queued': queued, 'max_jobs_running': max_jobs}
+    with open(command['port'], 'w') as f:
+        f.write(json.dumps(ret))
+
+def handle_configure(command):
+    global max_jobs
+    old_max_jobs = max_jobs
+    new_max_jobs = command['max_jobs']
+    ret = {'code': 0, 'status': 'OK',
+            'old_max_jobs_running': old_max_jobs, 'new_max_jobs_running': new_max_jobs,
+            'message': 'configuration successful'}
+
+    if new_max_jobs >= 0:
+        saturated.acquire()
+        max_jobs = new_max_jobs
+        saturated.notify()
+        saturated.release()
+    else:
+        ret['code'] = 2
+        ret['status'] = error
+        ret['message'] = 'invalid new max jobs running (must be >= 0)'
+    with open(command['port'], 'w') as f:
+        f.write(json.dumps(ret))
+
+def handle_invalid(command):
+    ret = {'code': 1, 'status': 'error', 'message': 'unknown command'}
+    with open(command['port'], 'w') as f:
+        f.write(json.dumps(ret))
 
 def handle_commands():
+    handlers = {'job': handle_submit_job,
+                'list': handle_get_listing,
+                'configure': handle_configure,
+                }
     while True:
         command = commands_q.get(block=True)
-        if command['type']=="job":
-            jobs_cv.acquire()
-            jobs.append(command['job'])
-            jobs_cv.notify()
-            jobs_cv.release()
-        elif command['type']=="ls":
-            jobs_cv.acquire()
-            print jobs # TODO: find a way to return feedback
-            jobs_cv.release()
+        prehooks(command)
+        if command['type'] not in handlers:
+            handle_invalid(command)
         else:
-            print 'Error: unknown command'
+            handlers[command['type']](command)
 
 def receive_commands_forever():
     while True:
         try:
             with open(pipe_name, 'r') as pipein:
                 commands = pipein.read().split('\n')
-                print commands
                 for command in commands:
-                    print command
                     if len(command) > 0:
                         commands_q.put(json.loads(command))
 
--- submit.py
+++ submit.py
@@ -5,41 +5,99 @@ import subprocess
 import shlex
 import json
 import itertools
+import argparse
 import re
 
 errors = {'eexists': 2}
 
-def escape_quotes(s):
-    return s.replace('"', '\\"')
 
-template ="""
+def submit(args, cmd_json, config):
+    if args.manager == 'all':
+        for manager in config['managers']:
+            args.manager = manager
+            submit(args, cmd_json, config)
+        return
+    elif args.manager == 'any':
+        raise Exception("not implemented yet")
+
+    settings = config['managers'][args.manager]
+    host = settings['host']
+
+    template = \
+"""
 cd %s;
 if ! mkfifo %s; then
     exit %d
 fi
 echo %s > %s;
-rm %s
+cat %s; # simple enough for now. just print the return message.
+rm %s # clear the port for later use
 """
+# TODO: ^ actually get stdout and deserialize json message from job manager
 
-host = sys.argv[1]
-cmd = sys.argv[2]
-config_file = sys.argv[3]
+    for port in itertools.count(1):
+        port_fifo = "%d.port" % port
+        cmd_json['port'] = port_fifo
+        # escaping arbitrary cmd line arguments in bash
+        # ref: http://qntm.org/bash
+        cmd_json_str = re.escape(json.dumps(cmd_json))
+        script = template % (settings['root'], port_fifo, errors['eexists'], cmd_json_str, settings['pipe'], port_fifo, port_fifo)
+        script = script.strip()
+        tocall = "ssh -A %s '%s'" % (host, script)
+        ret = subprocess.call(tocall, shell=True)
+        if ret == 0:
+            break
+        elif ret == errors['eexists']:
+            # then try a new port of this one was already in use
+            continue
+        else:
+            raise Exception("Trying to submit command, got error code %d" % ret)
 
-with open(config_file) as f:
-    config = yaml.safe_load(f)
+def handle_job(cmd_json, args, parser, config):
+    if args.cmd is None and args.cmd_file is None:
+        parser.error("command type %s requires either cmd or file" % args.type)
+    if args.cmd is not None:
+        cmd_json['run'] = args.cmd
+        submit(args, cmd_json, config)
+    if args.cmd_file is not None:
+        with open(args.cmd_file, 'r') as f:
+            for line in f:
+                cmd_json['run'] = line
+                submit(args, cmd_json, config)
 
-settings = config['hosts'][host]
+def handle_list(cmd_json, args, parser, config):
+    cmd_json['type'] = 'list'
+    submit(args, cmd_json, config)
 
-job = {'type': 'job',
-       'job' :  cmd}
+def handle_configure(cmd_json, args, parser, config):
+    cmd_json['type'] = 'configure'
+    if args.manager == 'any':
+        parser.error("this doesn't make sense; configuration should be specific")
+    if args.max_jobs is None:
+        parser.error("Configure command needs to specify new max jobs")
+    cmd_json['max_jobs'] = args.max_jobs
+    submit(args, cmd_json, config)
 
-for port in itertools.count(1):
-    port_fifo = "%d.port" % port
-    job['port'] = port_fifo
-    # escaping arbitrary cmd line arguments in bash
-    # ref: http://qntm.org/bash
-    job_str = re.escape(json.dumps(job))
-    script = template % (settings['root'], port_fifo, errors['eexists'], job_str, settings['pipe'], port_fifo)
-    script = script.strip()
-    if not subprocess.call("ssh -A %s '%s' >> submit.log 2>>submit.err" % (host, script), shell=True) == errors['eexists']:
-        break
+def main(args):
+    with open(args.config) as f:
+        config = yaml.safe_load(f)
+
+    if args.type not in command_type_handle:
+        parser.error("Command type must be one of %s" % command_type_handle.keys())
+
+    cmd_json = {'type': args.type, 'git': args.git, 'make': args.make}
+    command_type_handle[args.type](cmd_json, args, parser, config)
+
+if __name__=="__main__":
+    command_type_handle = {'job': handle_job, 'list': handle_list, 'configure': handle_configure}
+    parser = argparse.ArgumentParser(description="Submit job to job manager.")
+    parser.add_argument('manager')
+    parser.add_argument('type')
+    parser.add_argument('--config', dest='config', default='config.yaml')
+    parser.add_argument('--command', dest='cmd', default=None)
+    parser.add_argument('--command-file', dest='cmd_file', default=None)
+    parser.add_argument('--max-jobs-running', dest='max_jobs', default=None)
+    parser.add_argument('--git', dest='git', default=False, action='store_true')
+    parser.add_argument('--make', dest='make', default=False, action='store_true')
+    args = parser.parse_args()
+    main(args)
